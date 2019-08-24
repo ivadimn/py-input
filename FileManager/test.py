afdf403ae45c10693b6a895c3aee5f4f957436cd
ivadimn <ivadimn@mail.ru>
2019-08-24 19:32:15 +0300

    version test

--- FileManager/test.py
+++ FileManager/test.py
@@ -1,7 +1,11 @@
 import os
+import json
 my_list = ["aaaa", "bbbbb", "cccc", "ddddd", "eeeeee"]
 print(my_list[3:])
 
 
 s = "lkhlhlhlhk"
 print(os.name)
+print(s.title())
+print(s.upper())
+print(type(b'anb'))
