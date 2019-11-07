38d146d7c9f568c2dde99fcbbd2a9b5ae9c209b4
vadim <ivadimn@mail.ru>
2019-11-07 16:14:25 +0300

    create lesson 06

---
+++ algorithm/lesson06/common.py
@@ -0,0 +1,16 @@
+#  Функция для замера памяти
+import sys
+
+
+def show_sizeof(x, level=0):
+    print("\t" * level, x.__class__, sys.getsizeof(x), x)
+    if hasattr(x, '__iter__'):
+        if hasattr(x, 'items'):
+            for xx in x.items():
+                show_sizeof(xx, level + 1)
+        else:
+            for xx in x:
+                show_sizeof(xx, level + 1)
+
+print(sys.platform)
+print(sys.version)
