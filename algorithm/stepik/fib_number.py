e0f5d8176c0c8664548c5f78a13e75d3d65e137c
vadim <ivadimn@mail.ru>
2019-11-26 18:29:07 +0300

    new fib_number

--- algorithm/stepik/fib_number.py
+++ algorithm/stepik/fib_number.py
@@ -21,18 +21,74 @@ def fib_digit(n):
         last0, last1 = last1, lastn
     return lastn
 
+def period_pizano(m):
+    mods = [0, 1]
+    f1 = 0
+    f2 = 1
+    fn = f1 + f2
+    mod1 = fn % m
+    f1, f2 = f2, fn
+    fn = f1 + f2
+    print(fn, sep=" ")
+    mod2 = fn % m
+    while not (mod1 == mods[0] and mod2 == mods[1]):
+        mods.append(mod1)
+        mods.append(mod2)
+        f1, f2 = f2, fn
+        fn = f1 + f2
+        print(fn, sep=" ")
+        mod1 = fn % m
+        f1, f2 = f2, fn
+        fn = f1 + f2
+        print(fn, sep=" ")
+        mod2 = fn % m
+    return mods
+
+
+def period_pizano1(m):
+    mods = [0, 1]
+    f1 = 0
+    f2 = 1
+    fn = f1 + f2
+    mod = fn % m
+    while not (mods[len(mods) - 1] == mods[0] and mod == mods[1]):
+        mods.append(mod)
+        f1, f2 = f2, fn
+        fn = f1 + f2
+        mod = fn % m
+    return mods[:len(mods) - 1]
+
+
 def fib_mod(n, m):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    last0 = 0
-    last1 = 1
-    for i in range(2, n + 1):
-        lastn = (last0 + last1) % m
-        last0, last1 = last1, lastn
-    return lastn
+   mods = period_pizano1(m)
+   return mods[n % len(mods)]
 
-print(fib(100) % 7)
 
-print(fib_mod(100, 7))
+def matrix_mult(m1, m2):
+    pm = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                pm[i][j] += m1[i][k] * m2[k][j]
+    return pm
+
+
+M1 = [[1, 1], [1, 0]]
+M2 = [[1, 1], [1, 0]]
+a11 = M1[0][0]*M2[0][0] + M1[0][1]*M2[1][0]
+a12 = M1[0][0]*M2[0][1] + M1[0][1]*M2[1][1]
+a21 = M1[1][0]*M2[0][0] + M1[1][1]*M2[1][0]
+a22 = M1[1][0]*M2[0][1] + M1[1][1]*M2[1][1]
+print([[a11, a12], [a21, a22]])
+print(matrix_mult(M1, M2))
+
+
+
+m = 100000
+n = 1000000000000000000
+#print(fib(100) % 7)
+#print(fib(n))
+#print(period_pizano1(m))
+#print(fib_mod(n, m))
+
+#print(fib_mod(100, 7))
