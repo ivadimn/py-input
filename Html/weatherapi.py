1b22f1d58129012a5897eee04e0e8ba9c034c9b2
ivadimn <ivadimn@mail.ru>
2020-04-09 17:22:34 +0300

    new version

--- Html/weatherapi.py
+++ Html/weatherapi.py
@@ -1,5 +1,4 @@
 import requests
 
-req = requests.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e2405fa3b312d3a3398dad552d59f23f")
-text = req.text
-print(text)
+req = requests.get("http://api.github.com/users/ivadimn/repos")
+print(req.text)
