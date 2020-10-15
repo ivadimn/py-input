3632fa4a681fe10fc615456b228bada5aca5746d
vadim <ivadimn@mail.ru>
2020-10-15 15:20:15 +0300

    dublicate new

--- FileManager/dublicate.py
+++ FileManager/dublicate.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
+def filter_groups(groups):
+    print(len(groups))
+    tn = []
+    count =[]
+    for name, group in groups:
+        if (len(group) > 1):
+            tn.append(name)
+            count.append(len(group))
+    return tn, count
+
 df = pd.read_csv("nadym.csv", sep=";")
-df["key"] = range(1, 615)
-print(df.info)
-df.set_index("key", inplace=True)
-print(df.iloc(0))
+df_group = df.groupby("tn")
+tn_list, count_list = filter_groups(df_group)
+data = {"Таб.№": tn_list, "Количество": count_list}
+df_tn = pd.DataFrame(data)
+print(df_tn)
