993502b183132e98edaa8de0903fd252e544281b
vadim <ivadimn@mail.ru>
2021-09-23 05:53:01 +0300

    update

--- skillbox/basic/module08/task82.py
+++ skillbox/basic/module08/task82.py
@@ -1,3 +1,9 @@
+"""
+numbers = int(input("Введите количество чисел "))
+for number in range(1, numbers // 2 + 1):
+    n = number * 2
+    print("Число", n, " ** 3 равно", n ** 3)
+
 total_hours = int(input("Введите общее количество часов: "))
 cell = 1
 for hour in range(1, total_hours // 3 + 1):
@@ -7,4 +13,10 @@ for hour in range(1, total_hours // 3 + 1):
     print("Часов до конца эксперимента:", total_hours - hour * 3)
     print("-" * 20)
 print("Конец эксперимента!")
+"""
+
+numbers = int(input("Введите количество чисел "))
+for number in range(numbers // 2):
+    n = number * 2 + 1
+    print("Число", n, " ** 3 равно", n ** 3)
 
--- skillbox/basic/module08/task83.py
+++ skillbox/basic/module08/task83.py
@@ -1,4 +1,21 @@
-numbers = int(input("Введите количество чисел "))
-for number in range(numbers // 2):
-    n = number * 2 + 1
-    print("Число", n, " ** 3 равно", n ** 3)
+"""
+numbers = int(input("Введите количество чисел: "))
+for number in range(1, numbers + 1, 2):
+    print("Число", number," ** 3 равно", number ** 3)
+
+numbers = int(input("Введите число: "))
+summ = 0
+for number in range(1, numbers + 1, 5):
+    summ += number
+    print("Номер стула", number)
+print("Сумма:", summ)
+"""
+wake_up = int(input("Введите час пробуждения: "))
+water_volum = 0
+calories = 0
+for hour in range(wake_up, 23, 3):
+    water_volum += 1
+    calories += int(input("Сколько калориев съел? "))
+    print("Прошло:", hour, "часов")
+print("Всего выпито воды:", water_volum)
+print("Всего съедно калориев:", calories)
