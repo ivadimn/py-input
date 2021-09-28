e2be2ff25d0345cc0bb7502a799d40d0c8d43358
vadim <ivadimn@mail.ru>
2021-09-28 21:06:48 +0300

    update homework 09

--- skillbox/basic/module09/hwork09.py
+++ skillbox/basic/module09/hwork09.py
@@ -33,8 +33,53 @@ for _ in range(rows):
     print("=" * seats, end = " ")
     print("*" * meters, end = " ")
     print("=" * seats)
+
+length = 20
+width = 15
+way = " "
+pos_x = 10
+pos_y = 8
+while (way != "x") and (way != "X"):
+    print("[Программа]: Марсоход находится на позиции", pos_x,",", pos_y, ",введите команду:")
+    way = input("[Оператор (X дя завершения)]: ")
+    if (way == "w") or (way == "W"):
+        if pos_y > 0:
+            pos_y -= 1
+    elif (way == "s") or (way == "S"):
+        if pos_y < width:
+            pos_y += 1
+    elif (way == "a") or (way == "A"):
+        if pos_x > 0:
+            pos_x -= 1
+    elif (way == "d") or (way == "d"):
+        if pos_x < length:
+            pos_x += 1
+
+
+text = input("Введите строку: ")
+max_s = 0
+s_count = 0
+for symbol in text + "0":
+    if symbol == "s":
+        s_count += 1
+    else:
+        if s_count > 0:
+            if s_count > max_s:
+                max_s = s_count
+            s_count = 0
+print("Самая длинная последовательность:", max_s)
 """
 
-
+text = input("Введите текст: ")
+max_word = 0
+s_count = 0
+for symbol in text + " ":
+    if symbol == " ":
+        if s_count > max_word:
+            max_word = s_count
+        s_count = 0
+    else:
+        s_count += 1
+print("Длина самого длинного слова:", max_word)
 
 
