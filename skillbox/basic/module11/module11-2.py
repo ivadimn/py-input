058aa94a79fa5573f274c6991693f6fd79c5332d
vadim <ivadimn@mail.ru>
2021-10-08 16:16:08 +0300

    start module 11

---
+++ skillbox/basic/module11/module11-2.py
@@ -0,0 +1,18 @@
+"""
+stavka = float(input("Ваша ставка: "))
+koeff = float(input("Какой коэффициент: "))
+print("Потенциальный выигрыш: ", round(stavka * koeff, 2))
+
+
+age = int(input("Возраст сына: "))
+temperature = float(input("Температура тела: "))
+print("Отец подарит сыну: ", round(age * temperature * 1.5, 2))
+"""
+
+weight = float(input("Введите ваш вес: "))
+lenght = float(input("Введите ваш рост: "))
+bmi = weight / (lenght ** 2)
+print("Ваш  индекс массы тела:", round(, 2))
+
+
+
