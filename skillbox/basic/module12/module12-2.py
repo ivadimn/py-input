5a3f43303b8aa2d0aa6aa6c30428bdd15e528fa8
vadim <ivadimn@mail.ru>
2021-10-15 16:20:28 +0300

    module 12-2 finish

--- skillbox/basic/module12/module12-2.py
+++ skillbox/basic/module12/module12-2.py
@@ -1,4 +1,4 @@
-
+"""
 def greeting():
     print('Привет!')
     print('Добро пожаловать!')
@@ -8,3 +8,32 @@ while True:
     if a == 'Да':
         greeting()
     print('Следующий.\n')
+
+def howMuch():
+    a = int(input())
+    b = int(input())
+    print("Всего", a + b, "шт.")
+
+print("Сколько мешков рыбы и мяса?")
+howMuch()
+print("Сколько буханок белого и чёрного хлеба?")
+howMuch()
+print("Сколько вёдер воды и молока?")
+howMuch()
+"""
+
+first_name = "Иванов"
+second_name = "Василий"
+street = "Пушкина"
+house = 32
+
+def showData():
+    print("Фамилия: ", first_name)
+    print("Имя: ", second_name)
+    print("Улица: ", street)
+    print("Дом: ", house)
+    print()
+
+showData()
+showData()
+showData()
