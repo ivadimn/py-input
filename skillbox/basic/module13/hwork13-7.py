76c36902a83398dc9482853392ca5df223da588b
vadim <ivadimn@mail.ru>
2021-10-26 18:18:53 +0300

    task_7 update

--- skillbox/basic/module13/hwork13-7.py
+++ skillbox/basic/module13/hwork13-7.py
@@ -4,8 +4,17 @@ import math
 def danger_level(x):
     return x ** 3 - 3 * (x ** 2) - 12 * x + 10
 
-x = 0
-while  x < 4 :
+max_danger = float(input("Введите максимально допустимый уровень опасности: "))
+while max_danger <= 0:
+    print("Максимально допустимый уровень опасности должен быть больше 0. Повторите попытку :", end = "")
+    max_danger = float(input())
+
+d_min = 0
+d_max = 4
+while  True:
+    d_middle = (d_min + d_max) / 2
+    d = danger_level(d_middle)
+
     x += 0.0001
     level = danger_level(x)
     print(level)
