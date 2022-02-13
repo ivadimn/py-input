1beb3abc36b9331041ae1dbf3badbffe9d982569
vadim <ivadimn@mail.ru>
2022-02-13 22:03:50 +0300

    module 30 finished

--- skillbox/basic/module30/hwork30.py
+++ skillbox/basic/module30/hwork30.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Iterable
 from functools import reduce
+from collections import Counter
 
 """
 floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
@@ -38,7 +39,7 @@ numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
 
 result = list(map(lambda e1, e2: {e1: e2}, strings, numbers))
 print(result)
-"""
+
 
 def is_prime(number: int):
     for i in range(2, number):
@@ -46,13 +47,42 @@ def is_prime(number: int):
             return False
     return True
 
-p = []
-for i in range(1001):
-    if is_prime(i):
-        p.append(i)
-print(p)
+def alg(r: set, x: int) -> set:
+    print("x = {0}".format(x))
+    print("r = {0}".format(r))
+    s = set(range(x ** 2, n, x))
+    print("s = {0}".format(s))
+    ls = r - s
+    print("ls = {0}".format(ls))
+    if x in r:
+        result = ls
+    else:
+        result = r
+    print("result =  {0}".format(result))
+    return result
+
 
 #pl = [[max for num in range(2, max + 1) if (max % num == 0 and max == num)]  for max in range(2, 1001) if max % 2 != 0]
-pl = []
-for max in range(2, 1001):
-print(pl)
+
+n = 40
+pp = sorted(list(reduce(lambda s, x: s - set(range(x ** 2, n, x)) if x in s else s,
+                        range(2, int(n ** 0.5) + 1), set(range(2, n)))))
+print(pp)
+
+
+primes = [x for x in range(0, n)]
+for i in range(2, int(n ** 0.5) + 1):
+    for k in range(i * i, n, i):
+        primes[k] = 0
+
+print(list(filter(lambda elem: elem > 1, primes)))
+"""
+
+
+s1 = "aabbcccdd11"
+
+
+can_be_poly = lambda s: len(list(filter(lambda v: v % 2 > 0,Counter(s).values()))) <= 1
+
+
+print(can_be_poly(s1))
