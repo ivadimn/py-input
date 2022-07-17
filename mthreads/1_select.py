ae8cb06b4cb3d7c7dd11063cf07ea0c9017a3a2f
vadim <ivadimn@mail.ru>
2022-07-17 21:16:16 +0300

    update 20220717

--- mthreads/1_select.py
+++ mthreads/1_select.py
@@ -1,16 +1,20 @@
 import socket
+from select import select
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(("localhost", 5001))
 server_socket.listen()
 
-while True:
-    print("Before .accept()")
-    client_socket, addr = server_socket.accept()
-    client_socket.send("Link OK!!\n".encode())
-    print("Connection from", addr)
 
+def accept_connection(server_socket):
+    while True:
+        print("Before .accept()")
+        client_socket, addr = server_socket.accept()
+        client_socket.send("Link OK!!\n".encode())
+        print("Connection from", addr)
+
+def send_message(client_socket):
     while True:
         print("Before .recv()")
         request = client_socket.recv(4096)
@@ -19,5 +23,12 @@ while True:
         else:
             response = "Hello client\n".encode()
             client_socket.send(response)
-    print("Outside inner while loop")
     client_socket.close()
+
+
+def event_loop():
+  pass
+
+
+if __name__ == "__main__":
+    pass
