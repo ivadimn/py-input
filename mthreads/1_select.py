53e39661c470a58f02caa2b9dee31b419f3b502e
vadim <ivadimn@mail.ru>
2022-07-24 21:27:15 +0300

    update 20220717

--- mthreads/1_select.py
+++ mthreads/1_select.py
@@ -1,33 +1,41 @@
 import socket
 from select import select
 
+
+to_monitor = []
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(("localhost", 5001))
 server_socket.listen()
 
 
-def accept_connection(server_socket):
-    while True:
-        print("Before .accept()")
-        client_socket, addr = server_socket.accept()
-        print("Connection from", addr)
+def accept_connection(sock):
+    client_socket, addr = sock.accept()
+    to_monitor.append(client_socket)
+    print("Connection from", addr)
 
 def send_message(client_socket):
-    while True:
-        print("Before .recv()")
-        request = client_socket.recv(4096)
-        if not request:
-            break
-        else:
-            response = "Hello client\n".encode()
-            client_socket.send(response)
-    client_socket.close()
+    request = client_socket.recv(4096)
+    if request:
+        response = "Hello client\n".encode()
+        client_socket.send(response)
+    else:
+        client_socket.close()
 
 
 def event_loop():
-  pass
+  while True:
+      ready_to_read, _, _ = select(to_monitor, [], [])
+      for sock in ready_to_read:
+          if sock is server_socket:
+              accept_connection(sock)
+          else:
+              send_message(sock)
+
+
 
 
 if __name__ == "__main__":
-    pass
+    to_monitor.append(server_socket)
+    event_loop()
