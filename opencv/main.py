2d52e38ec9bca10e73cdb03fe4cb0457b30671e4
vadim <ivadimn@mail.ru>
2022-09-09 16:26:56 +0300

    add stepic oop

--- opencv/main.py
+++ opencv/main.py
@@ -1,20 +1,15 @@
 import cv2
 import numpy as np
+from params import read_flags
 
-img = cv2.imread("IMG_3930.JPG", cv2.IMREAD_COLOR)
-g, b, r = cv2.split(img)
-#yuv_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-gbr_img = cv2.merge((g, b, r))
-rbr_img = cv2.merge((r, b, r))
-cv2.imshow("original image", img)
-cv2.waitKey()
-cv2.imshow("gbr image", gbr_img)
-cv2.waitKey()
-cv2.imshow("rbr image", rbr_img)
-cv2.waitKey()
-#print([x for x in dir(cv2) if x.startswith('COLOR_')])
+for key, flag in read_flags.items():
+    img = cv2.imread("image1.jpg", flag)
+    cv2.imshow(key, img)
+    cv2.waitKey()
 
 
+
+"""
 num_rows, num_cols = img.shape[:2]
 translation_matrix = np.float32([[1, 0, 70], [0, 1, 110]])
 img_translation = cv2.warpAffine(img, translation_matrix, (num_cols + 70,
@@ -29,5 +24,6 @@ img_translation = cv2.warpAffine(img, translation_matrix, (num_cols * 2,
 img_rotation = cv2.warpAffine(img_translation, rotation_matrix, (num_cols * 2,
             num_rows * 2))
 cv2.imshow("affine rotate", img_rotation)
-cv2.waitKey()
 
+cv2.waitKey()
+"""
