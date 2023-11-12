257521443af156357ae17141f81ff166f41bf792
vadim <ivadimn@mail.ru>
2023-11-12 14:53:25 +0300

    get position from file

--- geo/geo.py
+++ geo/geo.py
@@ -1,5 +1,5 @@
 from typing import List
-import pandas as np
+import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 from time import sleep
@@ -14,20 +14,29 @@ def read_subjects(filename: str) -> List[str]:
         return subs
 
 
+def read_regions() -> dict:
+    regions = dict()
+    with open("regions.txt", "r") as f:
+        for line in f:
+            region, latitude, longitude = line.split(",")
+            regions[region] = (float(latitude), float(longitude),)
+        return regions
+
+
 def save_locations(locs: List):
     with open("locations.txt", "w") as f:
         for l in locs:
             f.write("{0},{1},{2}\n".format(l[0], l[1][0], l[1][1]))
 
 
-def map_locations(locations: np.Series):
-    #geolocator = Nominatim(user_agent='Gazprom_app', timeout=3)
+def map_locations(locations: pd.Series):
+    # geolocator = Nominatim(user_agent='Gazprom_app', timeout=3)
     unique_locations = locations.unique()
     map_locs = []
     for location in unique_locations:
         sleep(1)
         if location:
-            #position = geolocator.geocode(location)
+            # position = geolocator.geocode(location)
             position = Nominatim(user_agent='Gazprom_app', timeout=3).geocode(location)
             if position is not None:
                 map_locs.append((location, (position.latitude, position.longitude)))
@@ -35,12 +44,12 @@ def map_locations(locations: np.Series):
             else:
                 map_locs.append((location, (None, None)))
                 print(location, "None", "None")
-    #return np.DataFrame(map_locs, columns=['country_subject', 'position'])
+    # return pd.DataFrame(map_locs, columns=['country_subject', 'position'])
     return map_locs
 
 
 if __name__ == "__main__":
-    l = read_subjects("subjects.txt")
-    locs = np.Series(l)
-    save_locations(map_locations(locs))
-
+    # r = read_regions()
+    # print(r)
+    top_players = pd.read_excel("result1.xlsx")
+    print(top_players.head())
